18da83e276bec54adef8571c908dd873713cfbc8
Ori <oribibi10@gmail.com>
2020-12-10 15:33:30 +0200

    add new nav-bar and fix login button

--- server.js
+++ server.js
@@ -3,10 +3,22 @@ const path = require("path");
 
 const app = express();
 
-app.use("/static", express.static(path.resolve(__dirname, "frontend", "static")));
+app.use(express.json())
 
-app.get("/*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "frontend", "index.html"));
-});
+app.set('view engine', 'ejs')
 
+app.get('/', (req, res) => res.render('base', { isAuthenticated: false }))
+
+app.post('/index', (req, res) => {
+	// console.log('req', req)
+	res.render('pages/index', { username: undefined })
+})
+app.post('/about', (req, res) => {
+	// console.log('req', req)
+	res.render('pages/about')
+})
+app.post('/ContactUs', (req, res) => {
+	// console.log('req', req)
+	res.render('pages/ContactUs')
+})
 app.listen(process.env.PORT || 8081, () => console.log("Server running..."));
