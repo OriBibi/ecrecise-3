51beb4995e86552144583f92e212515908082345
Ori <oribibi10@gmail.com>
2020-12-27 11:04:25 +0200

    fix login issues

--- server.js
+++ server.js
@@ -11,16 +11,59 @@ var myCss = {
 };
 app.get('/', (req, res) => res.render('base', { isAuthenticated: false }))
 
+
+app.get('/index', (req, res) => {
+  console.log("3   " )
+  let username = req.query.username;
+  let password = req.query.password;
+  if(username === 'ori'){
+    console.log("here 1!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
+    if(password === 'bibi'){
+      console.log("here 2!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
+  res.render('pages/Main',{ myCss: myCss })
+    }}
+    else{
+      res.render('pages/index', { username: username,myCss: myCss })
+
+    }
+})
+
 app.post('/index', (req, res) => {
+	let username = req.query.username;
+  let password = req.query.password;
+	res.render('pages/index', { username: username,myCss: myCss })
+  console.log("what happen" + username)
+  
+    /*if (username === 'ori' && password == 'bibi' || username === 'ori' && password == 'oribibi' ||username === 'yigal' && password == 'fishler')
+    {
+
+	 console.log("login")
+	 res.render('pages/Main', { username: username,myCss: myCss })
+   } 
+  else{
+	res.send(username + ' not loggin! ');
+console.log("not login")
+	  res.render('pages/index', { username: username,myCss: myCss })
+   }*/
 	
-	res.render('pages/index', { username: undefined,myCss: myCss })
 })
 app.post('/about', (req, res) => {
 	// console.log('req', req)
 	res.render('pages/about',{ myCss: myCss })
 })
 app.post('/ContactUs', (req, res) => {
-	// console.log('req', req)
 	res.render('pages/ContactUs',{ myCss: myCss })
 })
+app.post('/Main', (req, res) => {
+  console.log("1  ")
+
+	res.render('pages/Main',{ myCss: myCss })
+})
+app.get('/Main', (req, res) => {
+  console.log("2  " )
+
+	res.render('pages/Main',{ myCss: myCss })
+})
+
 app.listen(process.env.PORT || 8080, () => console.log("Server running..."));
+
